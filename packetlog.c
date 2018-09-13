69ebdc96512a4e82bac614332ede772a1676d647
yfujieda <yfujieda@asl.cs.ritsumei.ac.jp>
2018-09-14 07:05:11 +0900

    buffer struct changed etc.. log_end to buffer_n->log_end

--- packetlog.c
+++ packetlog.c
@@ -1,6 +1,6 @@
 /*
 * my linux kernel version is 4.4.0 v127
-* target android hammerhead kernel version 3.3.0
+* target android hammerdata kernel version 3.3.0
 * android packet dump module
 */
 
@@ -68,16 +68,21 @@ unsigned long flags;
 /*
 * packet_num is all packet amount
 * log_end is array of packet end
-* matrix point base_matrix[0] , [BUFFER_SIZE]
-* buffer_array matrix[buffer_array]
+* buffer_n packet_buf[buffer_n]
 * flag initialization only ...
 */
 struct proc_dir_entry *proc_entry;
 unsigned int packet_num = 1;
-unsigned int log_end;
+unsigned int log_end[2];
 
-char **matrix, *base_matrix;
-int buffer_array;
+/*
+* packet_buf
+*
+*
+*/
+
+char *packet_buf;
+int buffer_ｎ;
 int i, j, n, m;
 bool main_flag = false; //初期化用
 
@@ -85,6 +90,12 @@ typedef struct packet_header{
   unsigned int packet_num;
   unsigned int packet_len;
 } packet_t;
+
+
+typedef struct packet_buf{
+  char *buf;
+  unsigned int log_end;
+} packet_buf_t;
 
 const char proc_buf[BUFFER_SIZE];
 //main module
@@ -108,9 +119,6 @@ static int proc_open(struct inode *node, struct file *fp){
   return 0;
 }
 
-
-
-
 /*
 * this function is proc filesystem read handler
 *  we want to try copying packet to __user buffer
@@ -121,39 +129,45 @@ static ssize_t proc_read(struct file *fp, char __user *buf, size_t size, loff_t 
 {
 
   char *p_temp=kmalloc(size, GFP_KERNEL);
-  int i, x;
+  int i;
+  static int temp;
   int state = 0;
   printk("read call count=%d\n",(int)size);
 
-  if(!buffer_array){
-    x=0;
+  if(buffer_n == 1){
+
+    spin_lock_irqsave(&log_lock, flags);
+    for(i=0; i < buffer_1->log_end; i++)
+    {
+      p_temp[i] = buffer_1->buf[i];
+    }
+    temp = buffer_1->log_end;
+    spin_unlock_irqrestore(&log_lock, flags);
   }else{
-    x=BUFFER_SIZE;
+    spin_lock_irqsave(&log_lock, flags);
+    for(i=0; i<log_end; i++)
+    {
+      p_temp[i] = buffer_2->buf[i];
+    }
+    temp = buffer_2->log_end;
+    spin_unlock_irqrestore(&log_lock, flags);
   }
-  
+
   spin_lock_irqsave(&log_lock, flags);
-  for(i=x; i<log_end; i++)
-  {
-    p_temp[i] = base_matrix[i];
-  }
-  spin_unlock_irqrestore(&log_lock, flags);
-  //ここにはスピンロック
-  spin_lock_irqsave(&log_lock, flags);
-  if (copy_to_user(buf, p_temp, sizeof(p_temp))) {
+  if (copy_to_user(buf, p_temp, temp) {
     kfree(p_temp);
-  goto out; 
+    goto out;
   }else{
-  kfree(p_temp);
-  } 
+    kfree(p_temp);
+  }
   spin_unlock_irqrestore(&log_lock, flags);
-  
+
   //stateが０の時つまりif state==0 と同値
   if(!state){
     //iは文字数を示す
-    state = i;
-
+    state = log_end;
   }
-out:
+  out:
   return state;
 }
 
@@ -165,6 +179,10 @@ static long proc_write(struct file *fp, const char *buf, size_t size, loff_t *of
   return size;
 }
 
+
+/*
+* ここでハンドラを登録する
+*/
 static struct file_operations example_proc_fops = {
   .owner = THIS_MODULE,
   .open = proc_open,
@@ -224,14 +242,15 @@ static unsigned int payload_dump(unsigned int hooknum,
   {
 
     packet_t packet_head;
+    unsigned int temp_packet_end;
     static char *buffer_p;
 
     //初期化系関数ここで初期化
     if (main_flag == false){
-      //base_matrixの先頭アドレスヲぶち込む
-      buffer_p = base_matrix;
-      buffer_array = 0;
-
+      //packet_bufの先頭アドレスヲぶち込む
+      buffer_p = buffer_1->buf;
+      buffer_n = 1;
+      temp_packet_end = 0;
       main_flag = true;
     }
 
@@ -240,36 +259,40 @@ static unsigned int payload_dump(unsigned int hooknum,
     packet_head.packet_len = skb->tail;
 
     //ここでバッファno書き込み開始ポインタを更新
-    if((BUFFER_SIZE - (int)(buffer_p - matrix[buffer_array])) < skb->tail)
+    if((BUFFER_SIZE - temp_packet_end < skb->tail)
     {
-      if(buffer_array == 0){
-        buffer_array = 1;
-        log_end = 0;
+      if(buffer_n == 1){
+        buffer_p = buffer_1->buf;
+        buffer_1->log_end = 0;
+        buffer_n = 1;
+        temp_packet_end = 0;
         printk("alternative1->0");
-      }else if(buffer_array == 1){
-      //nullireteta
-
-        buffer_array = 0;
-        log_end = 0;
+      }else if(buffer_n == 2){
+        buffer_p = buffer_2->buf;
+        buffer_2->log_end = 0;
+        buffer_n = 2;
+        temp_packet_end = 0;
         printk("alternative0->1");
       }else{
         printk(KERN_INFO"bufeer alternative is negative!!");
         return -2;
       }
-      buffer_p = matrix[buffer_array];
     }
 
     //ここで実際にバッファに挿入
     memcpy(buffer_p, &packet_head, sizeof(packet_head));
     buffer_p = buffer_p + sizeof(packet_head);
-    log_end = log_end + sizeof(packet_head);
-    memcpy(buffer_p , skb->head, skb->tail);
+    temp_packet_end = temp_packet_end + sizeof(packet_head);
+    memcpy(buffer_p , skb->data, skb->tail);
     buffer_p = buffer_p + skb->tail;
-    log_end = log_end + skb->tail;
+    temp_packet_end = temp_packet_end + skb->tail;
 
+    if(buffer_n == 1){
+      buffer_1->log_end = temp_packet_end;
+    }else if(buffer_n == 2){
+      buffer_2->log_end = temp_packet_end;
+    }
     printk("skb->tail:%d\n", skb->tail);
-    printk("packet1:%x\n", (long long unsigned int)*matrix[0]);
-    printk("packet2:%d\n", (int)*matrix[1]);
 
     packet_num++;
 
@@ -280,21 +303,23 @@ static unsigned int payload_dump(unsigned int hooknum,
 
   static int __init init_main(void)
   {
+    /*
+    * err is srr status if err is 1 , status negative
+    * rows is array element count
+    */
     int err;
 
-    //マトリックスの指す先にはパケットぶち込む箱の先頭アドレス
-    //ベースマトリックスの指す先にはバッファ全体
-
-    n = 2;
-
-    matrix = kmalloc(sizeof(char *) * n, GFP_KERNEL);
-    base_matrix = kmalloc(sizeof(char) * n * BUFFER_SIZE, GFP_KERNEL);
-    for (i=0;i<n;i++) {
-      matrix[i] = base_matrix + i * BUFFER_SIZE;
-    }
     /*
-    * こいつがパケットの書き出しの先頭を管理する
+    * this part is　reserved packet_buf(in callback func)
+    *
     */
+    packet_buf_t *buffer_1, *buffer_2;
+    packet_buf_t buffer[2];
+    /*buffer init*/
+    buffer_1 = buffer[0];
+    buffer_2 = buffer[1];
+    buffer_1->buf = kmalloc(sizeof(char *)*BUFFER_SIZE), GFP_KERNEL);
+    buffer_2->buf = kmalloc(sizeof(char *)*BUFFER_SIZE), GFP_KERNEL);
 
     nfhook.hook     = payload_dump;
     nfhook.hooknum  = 0;
@@ -308,7 +333,7 @@ static unsigned int payload_dump(unsigned int hooknum,
     if(err == 0){
       printk("create proc entry is succeed\n");
     }
-    return err;
+    return -err;
   }
 
   static void __exit cleanup_main(void)
@@ -317,9 +342,8 @@ static unsigned int payload_dump(unsigned int hooknum,
     printk("refused packetdump_mod");
     printk(KERN_INFO "%s\n", __FUNCTION__);
     proc_close();
-    kfree(base_matrix);
-    kfree(matrix);
-
+    kfree(buffer_1->buf);
+    kfree(buffer_2->buf);
 
   }
 
